475e852edb3e7d0755c8140a9724f32034ce7456
Мельников Денис <den-king-85@yandex.ru>
2023-05-17 21:32:37 +0300

    Добавил хосты в settings1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.30.11', '127.0.0.1', 'localhost', 'kittygrambydadyx.hopto.org']
+ALLOWED_HOSTS = ['158.160.30.11', '127.0.0.1', 'localhost', 'taskibydadyx.hopto.org']
 
 
 # Application definition
